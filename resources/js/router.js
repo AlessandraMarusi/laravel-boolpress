e4b01121ecdea01bab5838deee463cac8b62e3ab
AlessandraMarusi <alessandra.marusi@gmail.com>
2022-06-28 19:23:41 +0200

    Esercizio svolto

---
+++ resources/js/router.js
@@ -0,0 +1,45 @@
+import Vue from 'vue';
+
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+import HomeComponent from './pages/HomeComponent';
+import AboutComponent from './pages/AboutComponent';
+import PostsComponent from './pages/PostsComponent';
+import ContactComponent from './pages/ContactComponent';
+import SinglePostComponent from './pages/SinglePostComponent';
+
+const router = new VueRouter({
+    mode: "history", //per non fargli mettere il cancelletto
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            meta: {nome: 'Alessandra'},
+            component: HomeComponent
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: AboutComponent
+        },
+        {
+            path: '/posts',
+            name: 'posts',
+            component: PostsComponent
+        },
+        {
+            path: '/contact',
+            name: 'contact',
+            component: ContactComponent
+        },
+        {
+            path: '/posts/:slug',
+            name: 'single-post',
+            component: SinglePostComponent
+        },
+    ]
+});
+
+export default router;
